5131436de799679c809d75779d8250c66ec65575
Charlie Hagedorn <charlie@hagedorn.xyz>
2025-01-07 12:43:26 -0700

    Update run.sh -- updates disk-space tracking and adds rudimentary image-status

--- run.sh
+++ run.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 touch ~/it_works_for_real.txt
 
-df -h --total | tail > diskspace.dat
-rclone copy diskspace.dat gdrive:
+#df -h --total | tail > diskspace.dat
+#rclone copy diskspace.dat gdrive:
+
+#Disk-space tracking
+df -h --total > diskspace.txt
+date >> diskspace.txt
+rclone copy diskspace.txt gdrive:
+
+#Camera-operation monitoring
+echo "Cam 1" > last_images.txt
+ls -1 ~/cam1/ | tail -1 >> last_images.txt
+echo "Cam 2" >> last_images.txt
+ls -1 ~/cam2/ | tail -1 >> last_images.txt
+date >> last_images.txt
+rclone copy last_images.txt gdrive:
